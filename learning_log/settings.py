77c9e46ec98ce27c275069c517f7413894bf3ea5
evilGen <@>
2024-03-04 13:08:50 +0300

    добавлены новые модели

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -29,13 +29,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'learning_logs',
+
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learning_logs',
 ]
 
 MIDDLEWARE = [
