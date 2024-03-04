5e780eedd5d515412c9a988ba6fa8babd891e3fa
evilGen <@>
2024-03-04 16:38:44 +0300

    добавлены страницы тем

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello there!")
+    """Домашняя страница приложения Learning Log"""
+
+    # return HttpResponse("Hello there!")
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Выводит список тем."""
+
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+
+    return render(request=request, template_name="learning_logs/topics.html", context=context)
+
+
+def topic(request, topic_id):
+    """Выводит одну тему и все ее записи."""
+
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+
+    return render(request=request, template_name="learning_logs/topic.html", context=context)
